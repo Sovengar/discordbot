1ac9e88ccbe70e85a90a6352c4b4b4b7b30dc8e9
Sovengar <bubles_2009@hotmail.com>
2021-01-30 12:58:20 +0000

    gif

--- commands/gif.js
+++ commands/gif.js
@@ -5,9 +5,24 @@
 // https://youtu.be/B60Q74FHFBQ
 
 const fetch = require('node-fetch');
+const Discord = require('discord.js');
+
+// inside a command, event listener, etc.
+const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
 
 module.exports = async function(msg, args) {
-  let keywords = 'coding train';
+  msg.delete().catch(O_o=>{});
+  let keywords = 'random';
   if (args.length > 0) {
     keywords = args.join(' ');
   }
@@ -16,4 +31,6 @@ module.exports = async function(msg, args) {
   let json = await response.json();
   const index = Math.floor(Math.random() * json.results.length);
   msg.channel.send(json.results[index].url);
+  msg.channel.send(`by ${msg.author.username}`);
+  msg.channel.send(exampleEmbed);
 };
