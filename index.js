27a9f491506093712d973b67375b72e0c60f49bb
Sovengar <bubles_2009@hotmail.com>
2022-02-21 14:25:15 +0100

    Disabled replit server

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ console.log('Beep beep! 🤖');
 //Loads bot.js - Loads the Discord bot.
 require('./client/discordBot.js')
 
-///Hosting. Opens a server and a port to listen.
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('ok');
-});
-server.listen(3000);
+///Hosting with Replit 
+// Opens a server and a port to listen.
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200);
+//   res.end('ok');
+// });
+// server.listen(3000);
 //We need to keep doing pings to this server, i.e with UptimeRobot.com
