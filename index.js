0e224fbc50cf0a4b4985a20aa6f367ec5954633d
Jonathan <bubles_2009@hotmail.com>
2022-03-19 15:42:34 +0100

    added server to receive pings

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ console.log('Beep beep! 🤖');
 //Loads bot.js - Loads the Discord bot.
 require('./client/discordBot.js')
 
-///Hosting with Replit 
 // Opens a server and a port to listen.
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200);
-//   res.end('ok');
-// });
-// server.listen(3000);
 //We need to keep doing pings to this server, i.e with UptimeRobot.com
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('ok');
+});
+server.listen(3000);
